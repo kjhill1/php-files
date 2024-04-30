5a4ecfe6953c48cf0fb185fd9b481f138e20cacd
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2024-04-30 12:07:05 -0700

    PHP master files for protosite.
    Need to be tested.

---
+++ protosite-php-master-files/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" /><meta name='robots' content='noindex,nofollow' />
+    
+    <title><?php include('includes/titles.php'); ?> | Premium Design Works</title><!-- Begin Styles --><link href="http://www.sccc.premiumdw.com/examples/building-an-html-protosite-using-php/styles/master.css" rel="stylesheet" type="text/css" media="all" /><!-- End Styles --></head><body><!-- Begin Header --><div id="header">	<h1 id="logo"><a href="index.php"><img src="http://www.sccc.premiumdw.com/examples/building-an-html-protosite-using-php/images/logo.gif" alt="Premium Design Works" /></a></h1></div><!-- End Header --><?php include('includes/navigation.php'); ?><!-- Begin Middle --><div id="middle">
+
+    <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="keywords" content="resume, personal, portfolio, cv, card, responsive" />
+  <meta name="description" content="Resume / Personal / Portfolio Template" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Website</title>
+  <link rel="stylesheet" href="css/style-test-index.css"> 
+  <!-- <script src="js/script-test.js"></script> -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+</head>
